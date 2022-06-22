8311e42bf724289638453a111d786bddec2eb705
MrsUdeh <kelechiemeribe2@gmail.com>
2022-06-22 08:45:57 +0100

    crud

--- I4G021698KD0/blog/views.py
+++ I4G021698KD0/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+from I4G021698KD0.blog.models import Post
+from django.views.generic.edit import CreateView
+from .models import BlogApp
 # Create your views here.
