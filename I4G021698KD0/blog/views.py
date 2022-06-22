330c75f87c9bfab3b41e2e540d92f7d780a9d249
MrsUdeh <kelechiemeribe2@gmail.com>
2022-06-22 08:52:16 +0100

    crud

--- I4G021698KD0/blog/views.py
+++ I4G021698KD0/blog/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 from I4G021698KD0.blog.models import Post
 from django.views.generic.edit import CreateView
-from .models import BlogApp
+from .models import Post
 # Create your views here.
+class PostCreateView(CreateView):
+    model = Post
