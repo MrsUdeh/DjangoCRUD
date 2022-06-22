c21e5f9e794a50c97b030b44d44a9f4c8bb1c412
MrsUdeh <kelechiemeribe2@gmail.com>
2022-06-22 18:04:14 +0100

    crud

--- I4G021698KD0/blog/views.py
+++ I4G021698KD0/blog/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 
 from I4G021698KD0.blog.models import Post
-from django.views.generic.edit import ListView 
+from django.views.generic import ListView 
 from django.views.generic.edit import CreateView
-from django.views.generic.edit import DetailView
+from django.views.generic import DetailView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 from .models import Post
